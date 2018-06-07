ef6c6be3f3bf3a3339bc00d46cd78219468f3c05
mattl <mattl@cnuk.org>
2018-06-07 12:53:18 -0400

    Updated build script and template to include filename

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-rm *.html
+mkdir -p output
+rm output/*.html
 
 for file in *.md
 do
     PREFIX=$(basename "$file" | cut -d. -f1)
-    pandoc $file -T $PREFIX -V "pagetitle: $TITLE" -V "title: $TITLE" -w html5 -o "$PREFIX.html" --template ./templates/template.html
+    pandoc $file -T $PREFIX -V "pagetitle:$PREFIX" -V "title:$PREFIX" -w html5 -o "output/$PREFIX.html" --template ./templates/template.html
     #pandoc $file -w html -o "$PREFIX.html" --template ./templates/template.html
 done
 
